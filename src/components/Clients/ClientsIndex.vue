e3e8a8d24786f396f7016a6f9256fc2f68f1ec5f
Javier Roman Minguez <javierroman@javierroman.com>
2024-06-13 15:19:37 +0200

    Part 26. Install Tailwind and create login component

--- src/components/Clients/ClientsIndex.vue
+++ src/components/Clients/ClientsIndex.vue
@@ -5,10 +5,15 @@
 
     const fetchClients = async() => {
         try {
-            const response = await api.get('/api/clients')
+            const token = localStorage.getItem('userToken')
+            const response = await api.get('/api/clients', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
             clients.value = response.data.clients
         } catch (error) {
-            console.error('Error fetching items', error)
+            console.error('Error fetching clients', error)
             clients.value = []
         }
     }
@@ -17,26 +22,56 @@
 </script>
 
 <template>
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Last name</th>
-                <th>Email</th>
-                <th>Telephone</th>
-                <th>Observations</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="client in clients.data" :key="client.id">
-                <td>{{ client.id }}</td>
-                <td>{{ client.name }}</td>
-                <td>{{ client.last_name }}</td>
-                <td>{{ client.email }}</td>
-                <td>{{ client.telephone }}</td>
-                <td>{{ client.observations }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+                <table class="min-w-full">
+                    <thead class="bg-white border-b">
+                        <tr>
+                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                            Id
+                        </th>
+                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                            Name
+                        </th>
+                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                            Last name
+                        </th>
+                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                            Email
+                        </th>
+                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                            Telephone
+                        </th>
+                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                            Observations
+                        </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="client in clients.data" :key="client.id" class="bg-gray-100 border-b">
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ client.id }}</td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {{ client.name }}
+                            </td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {{ client.Last_name }}
+                            </td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {{ client.email }}
+                            </td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {{ client.telephone }}
+                            </td>
+                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                {{ client.observations }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
+    </div>
 </template>  
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ClientsIndex from '@/components/Clients/ClientsIndex.vue'
+import LoginUser from '@/components/LoginUser.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginUser
   },
   {
     path: '/clients',
